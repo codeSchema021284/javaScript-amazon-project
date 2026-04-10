d68288fd26a1e8b6e57f339774906fb55195f5fd
kweyila <myreandersonbtc@gmail.com>
2026-04-10 04:41:12 +0200

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -102,7 +102,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     html +=
 `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
      ${ischecked ? 'checked': ''}
         class="delivery-option-input"
@@ -134,3 +134,13 @@ document.querySelector('.js-order-summary')
         
         });
       });
+
+      document.querySelectorAll('.js-delivery-option')
+          .forEach((element) => {
+            element.addEventListener('click', () =>{
+            const {productId, deliveryOptionId} = element.dataset;
+           updateDeliveryOption(productId, deliveryOptionId);
+      
+
+            });
+          });
